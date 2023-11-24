422b5500153575517d9b5bdda322cf51d63ac13f
Yucel Cekinmez <cekinmezyucel@gmail.com>
2023-11-24 18:16:15 +0100

    feat: add next and previous buttons

--- TuneCruise/ContentView.swift
+++ TuneCruise/ContentView.swift
@@ -2,61 +2,90 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
+    // Radio stations
+    @State private var selectedStationIndex = 0
     let radioStations = [
-        RadioStation(name: "Ö3", streamURL: "https://orf-live.ors-shoutcast.at/oe3-q2a"),
-        RadioStation(name: "Wien", streamURL: "https://orf-live.ors-shoutcast.at/wie-q2a")
+        RadioStation(name: "OE3", streamURL: "https://orf-live.ors-shoutcast.at/oe3-q2a"),
+        RadioStation(name: "Radio Wien", streamURL: "https://orf-live.ors-shoutcast.at/wie-q2a")
     ]
     
     @State private var player: AVPlayer?
     @State private var isPlaying = false
-    @State private var selectedStation: RadioStation?
     
     var body: some View {
         VStack {
-            List(radioStations, id: \.self) { station in
+            // Clickable list of radio stations
+            List(radioStations, id: \.name) { station in
                 Button(action: {
-                    selectStation(station)
+                    playStation(streamURL: station.streamURL)
+                    if let index = radioStations.firstIndex(where: { $0.name == station.name }) {
+                        selectedStationIndex = index
+                    }
                 }) {
                     Text(station.name)
                 }
-                .foregroundColor(selectedStation == station ? .blue : .black)
             }
+            
             Spacer()
-            Button(action: {
-                togglePlayPause()
-            }) {
-                Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 50, height: 50)
-                    .foregroundColor(.blue)
+            
+            // Playback controls (Previous, Play/Pause, Next)
+            HStack {
+                Button(action: playPrevious) {
+                    Image(systemName: "backward.fill")
+                        .foregroundColor(.blue)
+                        .font(.title)
+                }
+                .disabled(selectedStationIndex == 0)
+                
+                Spacer()
+                
+                Button(action: togglePlayPause) {
+                    Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.blue)
+                }
+                .padding()
+                
+                Spacer()
+                
+                Button(action: playNext) {
+                    Image(systemName: "forward.fill")
+                        .foregroundColor(.blue)
+                        .font(.title)
+                }
+                .disabled(selectedStationIndex == radioStations.count - 1)
             }
             .padding()
         }
+        .padding()
     }
     
-    private func selectStation(_ station: RadioStation) {
-        guard let url = URL(string: station.streamURL) else {
-            print("Invalid URL")
-            return
+    func playStation(streamURL: String) {
+        stopCurrentStation()
+        
+        // Create a URL object from the streaming URL
+        guard let url = URL(string: streamURL) else {
+            fatalError("Invalid URL")
         }
         
-        if let selected = selectedStation, selected == station {
-            togglePlayPause()
-        } else {
-            selectedStation = station
-            playStream(url)
-        }
-    }
-    
-    private func playStream(_ url: URL) {
+        // Create an AVPlayerItem using the URL
         let playerItem = AVPlayerItem(url: url)
+        
+        // Initialize the AVPlayer with the player item
         player = AVPlayer(playerItem: playerItem)
         player?.play()
         isPlaying = true
     }
     
-    private func togglePlayPause() {
+    func stopCurrentStation() {
+        player?.pause()
+        player = nil
+        isPlaying = false
+    }
+    
+    func togglePlayPause() {
         if let player = player {
             if isPlaying {
                 player.pause()
@@ -66,9 +95,26 @@ struct ContentView: View {
             isPlaying.toggle()
         }
     }
+    
+    func playNext() {
+        if selectedStationIndex < radioStations.count - 1 {
+            selectedStationIndex += 1
+            let nextStation = radioStations[selectedStationIndex]
+            playStation(streamURL: nextStation.streamURL)
+        }
+    }
+    
+    func playPrevious() {
+        if selectedStationIndex > 0 {
+            selectedStationIndex -= 1
+            let previousStation = radioStations[selectedStationIndex]
+            playStation(streamURL: previousStation.streamURL)
+        }
+    }
 }
 
-struct RadioStation: Hashable {
+struct RadioStation: Identifiable {
+    let id = UUID()
     let name: String
     let streamURL: String
 }
