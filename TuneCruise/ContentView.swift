546d74210cc63108fc09e0d3b2c3da751aca09d6
Yucel Cekinmez <cekinmezyucel@gmail.com>
2023-11-24 18:07:42 +0100

    feat: implement radio player

--- TuneCruise/ContentView.swift
+++ TuneCruise/ContentView.swift
@@ -1,24 +1,50 @@
-//
-//  ContentView.swift
-//  TuneCruise
-//
-//  Created by Yuecel Cekinmez on 24.11.23.
-//
-
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    // Create an AVPlayer instance
+    @State private var player: AVPlayer?
+    @State private var isPlaying = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, TuneCruise!")
+            if let player = player {
+                // Display the play/pause button based on the playback state
+                Button(action: {
+                    if isPlaying {
+                        player.pause()
+                    } else {
+                        player.play()
+                    }
+                    isPlaying.toggle()
+                }) {
+                    Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.blue)
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            // Create a URL object from the streaming URL
+            // https://orf-live.ors-shoutcast.at/oe3-q2a
+            // https://orf-live.ors-shoutcast.at/wie-q2a
+            guard let url = URL(string: "https://orf-live.ors-shoutcast.at/wie-q2a") else {
+                fatalError("Invalid URL")
+            }
+            
+            // Create an AVPlayerItem using the URL
+            let playerItem = AVPlayerItem(url: url)
+            
+            // Initialize the AVPlayer with the player item
+            player = AVPlayer(playerItem: playerItem)
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
