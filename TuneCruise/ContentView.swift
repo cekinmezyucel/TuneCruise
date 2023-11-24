6cf346fb4a780d743886c2abab6ed311bce027a8
Yucel Cekinmez <cekinmezyucel@gmail.com>
2023-11-24 18:09:32 +0100

    feat: implement a radio playlist

--- TuneCruise/ContentView.swift
+++ TuneCruise/ContentView.swift
@@ -2,45 +2,75 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-    // Create an AVPlayer instance
+    let radioStations = [
+        RadioStation(name: "Ö3", streamURL: "https://orf-live.ors-shoutcast.at/oe3-q2a"),
+        RadioStation(name: "Wien", streamURL: "https://orf-live.ors-shoutcast.at/wie-q2a")
+    ]
+    
     @State private var player: AVPlayer?
     @State private var isPlaying = false
-
+    @State private var selectedStation: RadioStation?
+    
     var body: some View {
         VStack {
-            if let player = player {
-                // Display the play/pause button based on the playback state
+            List(radioStations, id: \.self) { station in
                 Button(action: {
-                    if isPlaying {
-                        player.pause()
-                    } else {
-                        player.play()
-                    }
-                    isPlaying.toggle()
+                    selectStation(station)
                 }) {
-                    Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(.blue)
+                    Text(station.name)
                 }
+                .foregroundColor(selectedStation == station ? .blue : .black)
             }
-        }
-        .onAppear {
-            // Create a URL object from the streaming URL
-            // https://orf-live.ors-shoutcast.at/oe3-q2a
-            // https://orf-live.ors-shoutcast.at/wie-q2a
-            guard let url = URL(string: "https://orf-live.ors-shoutcast.at/wie-q2a") else {
-                fatalError("Invalid URL")
+            Spacer()
+            Button(action: {
+                togglePlayPause()
+            }) {
+                Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.blue)
             }
-            
-            // Create an AVPlayerItem using the URL
-            let playerItem = AVPlayerItem(url: url)
-            
-            // Initialize the AVPlayer with the player item
-            player = AVPlayer(playerItem: playerItem)
+            .padding()
         }
     }
+    
+    private func selectStation(_ station: RadioStation) {
+        guard let url = URL(string: station.streamURL) else {
+            print("Invalid URL")
+            return
+        }
+        
+        if let selected = selectedStation, selected == station {
+            togglePlayPause()
+        } else {
+            selectedStation = station
+            playStream(url)
+        }
+    }
+    
+    private func playStream(_ url: URL) {
+        let playerItem = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: playerItem)
+        player?.play()
+        isPlaying = true
+    }
+    
+    private func togglePlayPause() {
+        if let player = player {
+            if isPlaying {
+                player.pause()
+            } else {
+                player.play()
+            }
+            isPlaying.toggle()
+        }
+    }
+}
+
+struct RadioStation: Hashable {
+    let name: String
+    let streamURL: String
 }
 
 struct ContentView_Previews: PreviewProvider {
