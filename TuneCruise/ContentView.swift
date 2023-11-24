a9d3b4ca3c276fe80859d78a33c7f959fa65bc1a
Yucel Cekinmez <cekinmezyucel@gmail.com>
2023-11-24 17:04:34 +0100

    Initial Commit

---
+++ TuneCruise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TuneCruise
+//
+//  Created by Yuecel Cekinmez on 24.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TuneCruise/TuneCruiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneCruiseApp.swift
+//  TuneCruise
+//
+//  Created by Yuecel Cekinmez on 24.11.23.
+//
+
+import SwiftUI
+
+@main
+struct TuneCruiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
